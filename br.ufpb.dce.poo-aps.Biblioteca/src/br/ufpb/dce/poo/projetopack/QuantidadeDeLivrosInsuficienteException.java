af681a190c421cba0d8c51737035d68d6b90f72f
Paulo Emílio <paulo.emilio@dce.ufpb.br>
2014-04-20 01:21:53 -0300

    Update QuantidadeDeLivrosInsuficienteException.java

--- br.ufpb.dce.poo-aps.Biblioteca/src/br/ufpb/dce/poo/projetopack/QuantidadeDeLivrosInsuficienteException.java
+++ br.ufpb.dce.poo-aps.Biblioteca/src/br/ufpb/dce/poo/projetopack/QuantidadeDeLivrosInsuficienteException.java
@@ -1,9 +1,6 @@
-package br.ufpb.dce.poo.projetopack;
+package br.ufpb.dce.poo.ExceptionsProject;
 
 public class QuantidadeDeLivrosInsuficienteException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
 	public QuantidadeDeLivrosInsuficienteException(String msg){
 		super(msg);
 	}
